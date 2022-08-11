fa29e8a2ac279642771a53c7a68b733772739555
MatiasChao <matias.chao@despegar.com>
2022-08-11 01:57:31 -0300

    tailwind added

--- server/app.js
+++ server/app.js
@@ -11,11 +11,7 @@ app.use(bodyParser.urlencoded({ extended: true }));
 
 app.use(cors())
 
-app.get('/', (req, res) => {
-  res.json('Hello World')
-})
-
-app.use('/api/articles', require('./routes/article')) // cual es el path y lo que voy a recibir
+app.use('/api/articles', require('./routes/article'))
 
 app.listen(process.env.PORT, () => {
     console.log('Server running on port', process.env.PORT)
